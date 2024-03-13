dc8f650cd4dcc983d0a5e828350b75469d9691b9
dey44and <iojaandrei11@gmail.com>
2024-03-13 23:05:18 +0200

    Implemented GUI app with python
    We use PyQt5 to use the interface for our app

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+import qdarkstyle
+from PyQt5.QtWidgets import QApplication
+from qtapp import MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    stylesheet = qdarkstyle.load_stylesheet_pyqt5()
+    app.setStyleSheet(stylesheet)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
